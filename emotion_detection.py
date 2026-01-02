ab6f5dcb596a6a9d64efa685df7712af167e85e6
zeynepsumer <zynp.sumer@gmail.com>
2026-01-02 07:02:00 -0500

    next_commit

--- emotion_detection.py
+++ emotion_detection.py
@@ -8,5 +8,24 @@ def emotion_detector(text_to_analyse):
 
     response = requests.post(url, json = myobj, headers=header)
     formatted_response = json.loads(response.text)
+    
+    emotion_predictions = formatted_response["emotionPredictions"][0]["emotion"]
 
-    return formatted_response
+    anger_score = emotion_predictions["anger"]
+    disgust_score = emotion_predictions["disgust"]    
+    fear_score = emotion_predictions["fear"]
+    joy_score = emotion_predictions["joy"]
+    sadness_score = emotion_predictions["sadness"]
+    
+    dominant_emotion = max(emotion_predictions, key = emotion_predictions.get)
+    
+    new_response = {
+        'anger': anger_score,
+        'disgust': disgust_score,
+        'fear': fear_score,
+        'joy': joy_score,
+        'sadness': sadness_score,
+        'dominant_emotion': dominant_emotion
+        }
+
+    return new_response
